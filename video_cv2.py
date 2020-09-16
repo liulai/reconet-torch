94bf2c5ffaf939123228e87492abd672629f8a89
Your Name <you@example.com>
2020-09-16 19:22:18 +0800

    update video_cv2.py

--- video_cv2.py
+++ video_cv2.py
@@ -75,7 +75,7 @@ def main():
     path = '.'
     os.chdir(path)
 
-    device = torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     print(args)
     if args.mode == 'video':
