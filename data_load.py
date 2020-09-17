de6cdc56fdd972fc2e0f97ff539d8500d4238b21
Your Name <you@example.com>
2020-09-17 17:45:53 +0800

    update data_load.py

--- data_load.py
+++ data_load.py
@@ -74,8 +74,8 @@ class MPIDataset2(Dataset):
         self.transform = transform
         self.dirlist = os.listdir(self.path + "/clean/")
         self.dirlist.sort()
-        self.dirlist = [item for item in self.dirlist if item != 'bandage_1']
-        print(self.dirlist)
+        self.dirlist = [item for item in self.dirlist if item.find('bandage_1')<0]
+        #print(self.dirlist)
         self.numlist = []
         for folder in self.dirlist:
             self.numlist.append(len(os.listdir(self.path + "/clean/" + folder + "/")))
